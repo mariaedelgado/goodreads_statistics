22614c821c3c4fe38dc651899d9f00c7279ad31e
mariaedelgado <maria.delgado.tejedor@gmail.com>
2023-01-20 18:51:07 +0100

    feat: add class Book
    Create script 'Bookshelf' to hold all the classes concerning the books. Defined class Book and its creator method

---
+++ src/bookshelf.py
@@ -0,0 +1,21 @@
+from utils.google_books_api import *
+
+class Book:
+    """
+    Class Book holds the information of a book.
+    """
+
+    def __init__(self, title=None, author=None, isbn=None, date_start=None, date_end=None, my_rating=None, average_rating=None):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.genre = get_genre_by_isbn(isbn)
+        self.my_rating = my_rating
+        self.average_rating = average_rating
+        self.date_start = date_start
+        self.date_end = date_end
+
+# class Bookshelf:
+#     """"
+#     Class Bookshelf holds all the books in the database.
+#     """"
